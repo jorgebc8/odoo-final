d2873e370949630c5c9845e48bfd637fdbbd22a5
jorgebc8 <59738556+jorgebc8@users.noreply.github.com>
2021-03-04 15:46:58 +0100

    Add files via upload

---
+++ phones/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'phones',
+    'version': '1.0',
+    'summary': 'phones models',
+    'website':'www.phonestoview.com',
+    'description': """
+                            Something that is not importent
+    """,
+    'depends': ['base'],
+    'data': [
+        'views/main_view.xml',
+    ],
+    'sequence': '0',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
