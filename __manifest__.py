d967f98ddc56ee3de670989c5a1acb3464d196fe
jorgebc8 <59738556+jorgebc8@users.noreply.github.com>
2021-05-24 17:57:11 +0200

    Delete __manifest__.py

--- __manifest__.py
+++
@@ -1,7 +0,0 @@
-{
-    'name': 'Facturas',
-    'description': 'Modulo para gestionar facturas',
-    'author': 'Jorge Bermudez Cervilla',
-    'application': True,
-    'data': ['views/producto.xml', 'views/facturas.xml', 'views/cliente.xml']
-}
